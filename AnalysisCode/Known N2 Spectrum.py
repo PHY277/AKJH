9a87bdb81c087054c5b976dee4e2acafec7e4de6
javonhall <javonhall03@icloud.com>
2025-04-30 16:42:51 -0400

    Y-axis and title change

--- AnalysisCode/Known N2 Spectrum.py
+++ AnalysisCode/Known N2 Spectrum.py
@@ -61,8 +61,9 @@ colors = [wavelength_to_rgb(wl) for wl in bin_centers]
 for i in range(len(bin_centers)):
     plt.bar(bin_centers[i], counts[i], width= 1, color=colors[i], edgecolor='black', linewidth=0.5)
 
+plt.ylabel('Lines')
 plt.xlabel('Wavelength (nm)')
 plt.xlim(380, 700)
-plt.title('Known N2 Spectrum (Colored by Wavelength)')
+plt.title('N2: Number of Lines vs Wavelengths (Colored by Wavelength)')
 
 plt.show()
